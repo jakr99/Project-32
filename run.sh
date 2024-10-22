9346ea3fa15e1293d9e77856e17693eed4f2f268
me <jkln3w@umsystem.edu>
2024-10-22 13:12:00 -0500

    removed input file

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if the input file is provided
-if [ $# -ne 1 ]; then
-  echo "Usage: ./run.sh <input_file>"
-  exit 1
-fi
 
-# Run the Python lexer program with input from the specified file
-python3 lexer.py3 < "$1"
+python3 lexer.py3 
 
